3e409b2f7054ac6338d345071ae51bfc17bb90b2
Eric Robsky Huntley <ericrobskyhuntley@proton.me>
2025-11-22 18:42:33 -0500

    wip, processing openalex

--- pfp/ingest.py
+++ pfp/ingest.py
@@ -1,8 +1,8 @@
 from pyalex import config, Works
 from itertools import chain
-from csv import DictWriter
-import json
-
+import pandas as pd
+import spacy
+import re
 
 def aii_to_text(index: dict) -> str:
     all_pos = [pos for positions in index.values() for pos in positions]
@@ -58,23 +58,65 @@ def parse_record(record: dict) -> dict:
     }
 
 
-def get_openalex(query: str, email: str) -> dict:
+def get_openalex(query: str, email: str, n_max: int = None) -> pd.DataFrame:
     config.email = email
     config.max_retries = 2
     config.retry_backoff_factor = 0.1
     config.retry_http_codes = [429, 500, 503]
 
     q = Works().search_filter(title_and_abstract=query)
-    results = [r for r in chain(*q.paginate(per_page=200))]
-    parsed = [parse_record(r) for r in results]
-    with open("output.csv", "w", newline="", encoding="utf-8") as file:
-        w = DictWriter(file, fieldnames=parsed[0].keys())
-        w.writeheader()
-        w.writerows(parsed)
+    results = [r for r in chain(*q.paginate(per_page=200, n_max=n_max))]
+    return pd.DataFrame([parse_record(r) for r in results])
 
-    with open("output.json", "w", encoding="utf-8") as file:
-        json.dump(results, file)
-    return parsed
+
+def clean_text(text, stopwords=None):
+    if stopwords is None:
+        stopwords = ["the", "a", "an", "and", "or", "but", "of", "to", "in"]
+    
+    text = text.strip()
+    
+    text = re.sub(r"(^|'s\b|'$)", "", text, flags=re.IGNORECASE).strip()
+    
+    text = re.sub(r"^[^\w]+|[^\w]+$", "", text)
+    
+    words = text.split()
+    
+    while words and words[0].lower() in stopwords:
+        words.pop(0)
+        
+    while words and words[-1].lower() in stopwords:
+        words.pop()
+
+    return " ".join(words).upper()
+
+
+def named_entities(df: pd.DataFrame, col: str, types: list = ["GPE"], model: str = "en_core_web_sm") -> pd.DataFrame:
+    """Detect named entities in column"""
+    nlp = spacy.load(model)
+    results = [
+        list(obj.text for obj in tup if obj.label_ in types)
+        for tup in [
+            i.ents for i in list(nlp.pipe(df[col], n_process=7))
+        ]
+    ]
+    df["_".join([col, 'ents'])] = [None if len(tup) == 0 else tup for tup in results]
+    
+    df["_".join([col, 'ents'])] = df["_".join([col, 'ents'])].apply(
+        lambda lst: [clean_text(s) for s in lst] if isinstance(lst, list) else []
+    )
+    return df
+
+test = get_openalex("reparations", "ehuntley@mit.edu", n_max = 10000)
+test2 = named_entities(test.dropna(subset=['title']), "title")
+
+t3 = (
+    test2.explode("title_ents")
+        .groupby(["year", "title_ents"])     # group by year and term
+        .size()                         # count occurrences
+        .unstack(fill_value=0) 
+        )
+
+t3.plot()
 
 
 if __name__ == "__main__":
